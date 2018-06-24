49f8bab478343993933d562b3cf5f8fab2acfbbe
Froilan Irizarry <froilan@froilanirizarry.me>
2018-06-24 00:41:42 -0400

    Refactored to make more readable and remove eslint errors

--- examples/two-rule-example.js
+++ examples/two-rule-example.js
@@ -1,61 +1,55 @@
-const fetch = require('node-fetch')
-const RulesEngine = require('../index')
+const fetch = require('node-fetch');
+const RulesEngine = require('../index');
 
 const rules = [
   {
-    field: "permissions.usageType",
-    validation: function (value) {
-      return value === 'openSource'
+    field: 'permissions.usageType',
+    validation: function(value) {
+      return value === 'openSource';
     },
-    outcome: function (obj) {
+    outcome: function(obj) {
       if (obj['repositoryURL']) {
-        obj.score = obj.score ? obj.score + 1 : 1
-        return obj
+        obj.score = obj.score ? obj.score + 1 : 1;
+        return obj;
       } else {
-        return obj
+        return obj;
       }
     }
   },
   {
-    field: "permissions.usageType",
-    validation: function (value) {
-      return value === 'governmentWideReuse'
+    field: 'permissions.usageType',
+    validation: function(value) {
+      return value === 'governmentWideReuse';
     },
-    outcome: function (obj) {
+    outcome: function(obj) {
       if (obj['repositoryURL']) {
-        return obj
+        return obj;
       } else {
-        obj.score = obj.score ? obj.score + 1 : 1
-        return obj
+        obj.score = obj.score ? obj.score + 1 : 1;
+        return obj;
       }
     }
   }
-]
+];
 
-const engine = new RulesEngine(rules)
+const engine = new RulesEngine(rules);
 
 fetch('https://open.gsa.gov/code.json')
-  .then(response => response.json())
-  .then(json => json.releases)
-  .then(releases => {
-    let openSourceCount = 0
-    let governmentWideReuseCount = 0
-    for (repo of releases) {
-      engine.execute(repo)
-        .then(result => {
-          if (result.permissions.usageType === 'openSource') {
-            openSourceCount += 1
-          }
-          if (result.permissions.usageType === 'governmentWideReuse') {
-            governmentWideReuseCount += 1
-          }
-
-          return {
-            openSourceCount,
-            governmentWideReuseCount
-          }
-        })
-        .then(result => console.log(result))
+  .then((response) => response.json())
+  .then((json) => json.releases)
+  .then(async (releases) => {
+    let openSourceCount = 0;
+    let governmentWideReuseCount = 0;
+    for (let repo of releases) {
+      let result = await engine.execute(repo);
+      if (result.permissions.usageType === 'openSource') {
+        openSourceCount += 1;
+      }
+      if (result.permissions.usageType === 'governmentWideReuse') {
+        governmentWideReuseCount += 1;
+      }
     }
-  })
-
+    /* eslint-disable */
+    console.log(`openSourceCount: ${openSourceCount}, governmentWideReuseCount: ${governmentWideReuseCount}`);
+    /* eslint-enable */
+  });
